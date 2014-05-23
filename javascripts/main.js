0a90d628fb5955a52d4b0ce5376c2acd05d226a9
Simplex <ilya.sumb+simplex-company@gmail.com>
2014-05-23 22:54:31 +0700

    some changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 function mkNavbar(active) {
   var navigation = [
-    { type: 0, title: "Главная", link: "/arbk" },
+    { type: 0, title: "Главная", link: "/" },
     { type: 1, title: "О компании", link: "#", label: "l0", subnav: [
         { title: "Руководсво и подразделения", link: "#" },
         { title: "Лицензии и сертификаты", link: "#" },
@@ -8,18 +8,18 @@ function mkNavbar(active) {
       ]},
     { type: 1, title: "Услуги", link: "#", label: "l1", subnav: [
         { title: "Оценка", link: "#"},
-        { title: "Экспертиза", link: "#"},
+        { title: "Экспертиза", link: "/service/expertise/cte.html"},
         { title: "Патентование", link: "#"}
       ]},
     { type: 1, title: "Структура организации", link: "#", label: "l2", subnav: [
         { title: "Отдел оценки недвижимости", link: "#"},
-        { title: "Отдел строительно-технической экспертизы", link: "#"},
+        { title: "Отдел строительно-технической экспертизы", link: "/structure/cted.html"},
         { title: "Отдел судебной экспертизы", link: "#"},
         { title: "Отдел патентования и лицензирования", link: "#"},
         { title: "Юридический отдел", link: "#"}
       ]},
     { type: 0, title: "Тарифы", link: "#"},
-    { type: 0, title: "Контакты", link: "contact.html"}
+    { type: 0, title: "Контакты", link: "/contact.html"}
   ];
 
   var result = '<div class="navbar navbar-inverse navbar-fixed-top" role="navigation"> \
@@ -60,5 +60,38 @@ function mkNavbar(active) {
 
   $(document).ready(function() {
     $('#_navbar').append(result);
+  });
+}
+
+function mkExpertiseNav(exp) {
+  var navigation = [
+    { title: "Строительно-техническая экспертиза", link: "cte.html" },
+    { title: "Судебная экспертиза", link: "#"},
+    { title: "Оценочная экспертиза", link: "#"},
+    { title: "Патентоведческая экспертиза", link: "#"},
+    { title: "Автотовароведческая экспертиза", link: "#"},
+    { title: "Землеустроительная экспертиза", link: "#"},
+    { title: "Экспертиза отчетов", link: "#"},
+    { title: "Экспертиза проектной документации и (или) инженерных изысканий", link: "#"},
+    { title: "Составление проекта по реконструкции отдельных частей здания", link: "#"},
+    { title: "Почерковедческая экспертиза", link: "#"},
+    { title: "Экспертиза ущерба и затопления", link: "#"},
+    { title: "Рецензирование экспертных заключений", link: "#"},
+    { title: "Экспертиза проектно-сметной документации", link: "#"},
+    { title: "Экспертиза качества строительных и ремонтных работ", link: "#"}
+  ];
+
+  var result = '<ul class="nav nav-pills nav-stacked">'
+  $.each(navigation, function(i, item) {
+    result += '<li';
+    if(item.title == exp) {
+      result += ' class="active"';
+    }
+    result += '><a href="' + item.link + '">' + item.title + '</a></li>';
+  });
+  result += '</ul>';
+
+  $(document).ready(function () {
+    $('#_expnav').append(result);
   });
 }
